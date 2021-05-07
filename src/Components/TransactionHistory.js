23b30cd1947f86088ea9c1ae8014c525ba484f31
Jinghang <n.jing.hang@gmail.com>
2021-05-07 17:16:34 +0800

    Added Single Transaction Page

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ViewTransaction from './App';
 import ViewAccountDetails from './ViewAccountDetails'
-import AddTransactionPage from './AddTransaction';
+//import AddTransactionPage from './AddTransaction';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <ViewAccountDetails /><br></br>
-    <AddTransactionPage /><br></br>
     <ViewTransaction />
   </React.StrictMode>,
   document.getElementById('root')
