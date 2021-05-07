883b93c79881a4b90066eb887824eafbf39f5bd0
RajkiranChobey <82642869+RajkiranChobey@users.noreply.github.com>
2021-05-07 12:54:49 +0800

    updated components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Link,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Login}></Route>
+          <Route exact path="/Login" component={Login}></Route>
+          <Route exact path="/BuySellAsset" component={ViewTransaction}></Route>
+          <Route
+            exact
+            path="/TransactionHistory"
+            component={ViewAccountDetails}
+          ></Route>
+          <Route exact path="/Logout" component={Logout}></Route>
+          <Route exact path="/UserInfo" component={AddTransaction}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
