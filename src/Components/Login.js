c7038507f1d04833d1ea7a6875c608ad14b84d87
RajkiranChobey <82642869+RajkiranChobey@users.noreply.github.com>
2021-05-07 11:41:23 +0800

    edit login

---
+++ src/Components/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+export default class LoginPage extends Component {
+  render() {
+    return (
+      <div className="formCenter">
+        <form className="formField" onSubmit={this.handleSubmit}>
+          <h3>Login</h3>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              onChange={(e) => (this.email = e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              onChange={(e) => (this.password = e.target.value)}
+            />
+          </div>
+
+          <button className="btn btn-primary btn-block">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
